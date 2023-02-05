af8725c7a6b5643b4fbb4c2332e70b9ac5b461f1
ivansnow02 <ivansnow02@gmail.com>
2023-02-05 16:29:38 +0800

    init

--- src/test/java/com/is/ApplicationTests.java
+++ src/test/java/com/is/ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Springboot02BaseConfigurationApplicationTests {
+class ApplicationTests {
 
     @Test
     void contextLoads() {
